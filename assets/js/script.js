35d97e402907c69e59224d791097c8e3e316ce12
Sandy McCabe <sandytech13@gmail.com>
2021-10-03 11:00:58 -0500

    basic layout

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+const questions = [
+    {Q: "Commonly used data types DO NOT include", 
+    a1: "1. strings", 
+    a2: "2. booleans",
+    a3: "3. alerts",
+    a4: "4. numbers",
+    answer: "3"},
+    
+    {Q: "A very useful tool used during development and debugging for printing content to the debugger is:", 
+    a1: "1. JavaScript", 
+    a2: "2. terminal/bash",
+    a3: "3. for loops",
+    a4: "4. console.log",
+    answer: "4"},
+
+    {Q: "The condition with an if / else statement is enclosed with _____", 
+    a1: "1. quotes",
+    a2: "2. curly brackets",
+    a3: "3. parenthesis",
+    a4: "4. square brackets",
+    answer: "3"},
+
+    {Q: "Arrays in JavaScript can be used to store ___", 
+    a1: "1. numbers and strings", 
+    a2: "2. other arrays",
+    a3: "3. booleans",
+    a4: "4. all of the above",
+    answer: "4"},
+    
+    {Q: "String values must be enclosed within ___ when being assigned to variables.", 
+    a1: "1. commas", 
+    a2: "2. curley brackets",
+    a3: "3. quotes",
+    a4: "4. parenthesis",
+    answer: "3"},
+]
+
+const highScores = [
+    //initials, score
+]
